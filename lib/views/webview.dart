652fdb102a1a2a26a2dada2812fb65d587095b84
falstack <icesilt@outlook.com>
2019-10-28 20:34:56 +0800

    add: webview support

--- lib/views/webview.dart
+++ lib/views/webview.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 
 class Webview extends StatefulWidget {
   Webview({Key key}) : super(key: key);
@@ -8,12 +9,28 @@ class Webview extends StatefulWidget {
 }
 
 class _WebviewState extends State<Webview> {
+  final flutterWebviewPlugin = new FlutterWebviewPlugin();
+
+  @override
+  void initState() {
+    super.initState();
+
+    flutterWebviewPlugin.onUrlChanged.listen((String url) {
+      print('url change:$url');
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: new Text('webview')
+      ),
+      body: WebviewScaffold(
+        url: 'https://www.baidu.com',
+        withZoom: false,
+        withLocalStorage: true,
+        withJavascript: true,
       ),
     );
   }
