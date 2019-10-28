029135eb26df892f567aa932ca186f2da041ebee
falstack <icesilt@outlook.com>
2019-10-28 19:44:31 +0800

    add: tab-container

--- lib/views/main-note.dart
+++ lib/views/main-note.dart
@@ -10,20 +10,55 @@ class MainNote extends StatefulWidget {
 }
 
 class _MainNoteState extends State<MainNote> {
+  static const tabs = [
+    '关注',
+    '推荐',
+    '漫友圈'
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('发现')
-      ),
-      floatingActionButton: new FloatingActionButton(
-        tooltip: '发帖', // used by assistive technologies
-        child: new Icon(Icons.add),
-        backgroundColor: Theme.of(context).primaryColor,
-        onPressed: () {
-          App.router.navigateTo(context, '/webview', transition: TransitionType.native);
-        },
+    return new DefaultTabController(
+      length: tabs.length,
+      child: new Scaffold(
+        appBar: AppBar(
+          bottom: new TabBar(
+            isScrollable: true,
+            indicatorColor: Theme.of(context).primaryColor,
+            tabs: tabs.map((item) {
+              return new Tab(
+                  text: item
+              );
+            }).toList(),
+          ),
+        ),
+        body: new TabBarView(
+          children: tabs.map((item) {
+            return new Padding(
+                padding: const EdgeInsets.all(16.0),
+                child:  new Card(
+                  color: Colors.white,
+                  child: new Center(
+                    child: new Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        new Text(item),
+                      ],
+                    ),
+                  ),
+                )
+            );
+          }).toList(),
+        ),
+        floatingActionButton: new FloatingActionButton(
+          tooltip: '发帖', // used by assistive technologies
+          child: new Icon(Icons.add),
+          backgroundColor: Theme.of(context).primaryColor,
+          onPressed: () {
+            App.router.navigateTo(context, '/webview', transition: TransitionType.native);
+          },
+        ),
       ),
     );
   }
