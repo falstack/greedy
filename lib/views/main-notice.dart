1a432810c50984fa8fdc43c69bfc7a5f978497a5
falstack <icesilt@outlook.com>
2019-10-29 20:52:47 +0800

    add grid layout

--- lib/views/main-notice.dart
+++ lib/views/main-notice.dart
@@ -7,13 +7,32 @@ class MainNotice extends StatefulWidget {
   _MainNoticeState createState() => _MainNoticeState();
 }
 
-class _MainNoticeState extends State<MainNotice> {
+List<Container> _buildGridTileList(int count) {
 
+  return new List<Container>.generate(
+      count,
+          (int index) =>
+      new Container(child: new Image.asset('assets/images/pic${index}.jpg')));
+}
+
+Widget _buildGrid() {
+  return new GridView.extent(
+      maxCrossAxisExtent: 150.0,
+      padding: const EdgeInsets.all(4.0),
+      mainAxisSpacing: 4.0,
+      crossAxisSpacing: 4.0,
+      children: _buildGridTileList(10));
+}
+
+class _MainNoticeState extends State<MainNotice> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('消息')
+      ),
+      body: new Center(
+        child: _buildGrid(),
       ),
     );
   }
