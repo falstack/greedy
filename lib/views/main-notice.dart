0ee9dd1fb8e05e50700c9d42c57dabae01983fe5
falstack <icesilt@outlook.com>
2019-12-05 09:58:13 +0800

    add: message list

--- lib/views/main-notice.dart
+++ lib/views/main-notice.dart
@@ -1,7 +1,5 @@
-import 'package:fluro/fluro.dart';
 import 'package:flutter/material.dart';
-import 'package:greedy/config/app.dart';
-import 'package:greedy/views/index.dart' show demoWidgets;
+import 'package:greedy/components/ChatMessage.dart';
 
 class MainNotice extends StatefulWidget {
   MainNotice({Key key}) : super(key: key);
@@ -11,6 +9,44 @@ class MainNotice extends StatefulWidget {
 }
 
 class _MainNoticeState extends State<MainNotice> {
+  final List<ChatMessage> _messages = <ChatMessage>[];
+  final TextEditingController _textController = new TextEditingController();
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+    ChatMessage message = new ChatMessage(
+      text: text,
+    );
+    setState(() {
+      _messages.insert(0, message);
+    });
+  }
+
+  Widget _buildTextComposer() {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: Row(
+        children: <Widget>[
+          Flexible(
+            child: TextField(
+              controller: _textController,
+              onSubmitted: _handleSubmitted,
+              decoration: InputDecoration.collapsed(
+                hintText: "Send a message"
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 4.0),
+            child: IconButton(
+              icon: Icon(Icons.send),
+              onPressed: () => _handleSubmitted(_textController.text)
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,26 +54,25 @@ class _MainNoticeState extends State<MainNotice> {
       appBar: AppBar(
         title: Text('消息')
       ),
-      body: Container(
-        child: ListView.builder(
-          itemBuilder: (context, index) {
-            return InkWell(
-              onTap: () {
-                App.router.navigateTo(context, demoWidgets[index]['path'], transition: TransitionType.native);
-              },
-              child: Card(
-                child: Container(
-                  alignment: Alignment.centerLeft,
-                  margin: EdgeInsets.symmetric(horizontal: 10),
-                  height: 50,
-                  child: Text(demoWidgets[index]['name']),
-                ),
-              ),
-            );
-          },
-          itemCount: demoWidgets.length,
-        ),
-      ), //
+      body: Column(
+        children: <Widget>[
+          Flexible(
+            child: ListView.builder(
+              padding: EdgeInsets.all(8.0),
+              reverse: true,
+              itemBuilder: (_, int index) => _messages[index],
+              itemCount: _messages.length,
+            ),
+          ),
+          Divider(height: 1.0),
+          Container(
+            decoration: BoxDecoration(
+              color: Theme.of(context).cardColor
+            ),
+            child: _buildTextComposer(),
+          ),
+        ],
+      ),
     );
   }
 }
